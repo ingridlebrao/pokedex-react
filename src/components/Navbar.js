5619e3ab23cf94127501d2ce7df251ece27a493d
Ingrid Lebrão <ingridlebrao@gmail.com>
2022-06-23 17:54:29 -0300

    create navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
+import { useContext } from 'react';
+import FavoriteContext from '../context/favoritesContext';
+
 const Navbar = () => {
+  const { favoritePokemons } = useContext(FavoriteContext);
   const logoNav =
     'https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png';
   return (
-    <div className="navbar">
-      <img alt="PokeApi" src={logoNav} className="navbar__logo" />
-    </div>
+    <nav className="nav">
+      <div className="navbar">
+        <img alt="PokeApi" src={logoNav} className="navbar__logo" />
+      </div>
+      <div className="navbar-favorites">
+        <span className="material-icons">favorite</span>
+        {favoritePokemons.length}{' '}
+      </div>
+    </nav>
   );
 };
 
